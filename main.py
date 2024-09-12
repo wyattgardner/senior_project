af290052da4fd42dd4345e7c95661ab10a1067e6
wyattgardner <wyattgardner1@gmail.com>
2024-09-12 17:01:53 -0400

    added LCD

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ import sys
 sys.path.append("")
 
 from micropython import const
-
 import uasyncio as asyncio
 import aioble
 import bluetooth
+import machine
+import RGB1602
+
+# Enables logging to log.txt in root directory of Pico W
+# For testing/debugging purposes only, will eventually fill the board's 2 MB flash memory
+ENABLE_LOGGING = const(False)
+# GPIO Pins used for sensors
+# LCD: SDA is on GPIO4, and SCL is on GPIO5
+MQ_9 = machine.Pin(2, machine.Pin.OUT)
+MQ_135 = machine.Pin(3, machine.Pin.OUT)
+#LCD = RGB1602.RGB1602(16, 2)
 
 # org.bluetooth.service.environmental_sensing
 _ENV_SENSE_UUID = bluetooth.UUID(0x181A)
@@ -18,35 +28,71 @@ _ENV_SENSE_CH4_UUID = bluetooth.UUID("ef090001-2ec0-4cd4-8f5a-51de99e65ecb")
 _ENV_SENSE_RECV_UUID = bluetooth.UUID("ef090002-2ec0-4cd4-8f5a-51de99e65ecb")
 # org.bluetooth.characteristic.gap.appearance.xml
 _ADV_APPEARANCE_GENERIC_SENSOR = const(0x0540)
-
 # How frequently to send advertising beacons in microseconds
 _ADV_INTERVAL_US = const(250_000)
 
 # Register GATT server.
 env_service = aioble.Service(_ENV_SENSE_UUID)
 co_characteristic = aioble.Characteristic(
-    env_service, _ENV_SENSE_CO_UUID, read=True, notify=True
+    env_service, _ENV_SENSE_CO_UUID, read=True, notify=True, initial="CO"
 )
 ch4_characteristic = aioble.Characteristic(
-    env_service, _ENV_SENSE_CH4_UUID, read=True, notify=True
+    env_service, _ENV_SENSE_CH4_UUID, read=True, notify=True, initial="CH4"
 )
 recv_characteristic = aioble.Characteristic(
-    env_service, _ENV_SENSE_RECV_UUID, write=True, read=True, notify=True, capture=True
+    env_service, _ENV_SENSE_RECV_UUID, write=True, read=True, notify=True, capture=True, initial="RECEIVE"
 )
 aioble.register_services(env_service)
+
+if ENABLE_LOGGING:
+    log_file = open('log.txt', 'a')
+
+def _logger(*args, **kwargs):
+    data = ' '.join(str(arg) for arg in args)
+
+    print(data)
+
+    if ENABLE_LOGGING:
+        log_file.write(data + '\n')
+        log_file.flush()
+
+# Write to LCD and set backlight
+def write_to_LCD(line1: str, line2: str, backlight: str):
+    # Truncate strings to 16 characters - LCD is 16x2
+    line1 = line1[:16]
+    line2 = line2[:16]
+
+    # Write both lines to LCD
+    LCD.setCursor(0, 0)
+    LCD.printout(line1)
+    LCD.setCursor(0, 1)
+    LCD.printout(line2)
+
+    # Set backlight
+    if (backlight == "normal"):
+        LCD.setRGB(255, 255, 255)
+    elif (backlight == "warning"):
+        LCD.setRGB(255, 255, 0)
+    elif (backlight == "alert"):
+        LCD.setRGB(255, 0, 0)
+    else:
+        LCD.setRGB(255, 255, 255)
 
 async def transmit_data():
     while True:
         co_characteristic.write("test1")
+        await asyncio.sleep_ms(50)
         ch4_characteristic.write("test2")
-        await asyncio.sleep_ms(250)
+        await asyncio.sleep_ms(50)
 
 async def receive_data():
     while True:
         connection, data = await recv_characteristic.written()
+        await asyncio.sleep_ms(50)
         recv_characteristic.notify(connection, "Received!")
-        print("Data received:")
-        print(data.decode())
+        await asyncio.sleep_ms(50)
+        _logger("Data received:")
+        _logger(data.decode())
 
 # Serially wait for connections. Don't advertise while a central is
 # connected.
@@ -58,17 +104,26 @@ async def peripheral_task():
             services=[_ENV_SENSE_UUID],
             appearance=_ADV_APPEARANCE_GENERIC_SENSOR,
         ) as connection:
-            print("Connection from", connection.device)
+            _logger("Connection from:", connection.device)
             await connection.disconnected()
+            _logger("Device disconeccted:", connection.device)
+            await asyncio.sleep_ms(100)
 
-
-# Run both tasks.
+# Run tasks.
 async def main():
-    asyncio.create_task(transmit_data())
-    asyncio.create_task(peripheral_task())
-    asyncio.create_task(receive_data())
-    while True:
-        await asyncio.sleep(1)
+    try:
+        tasks = [
+            asyncio.create_task(peripheral_task()),
+            asyncio.create_task(transmit_data()),
+            asyncio.create_task(receive_data()),
+        ]
+        await asyncio.gather(*tasks)
 
+    except Exception as e:
+        _logger('An error occurred: ' + str(e))
+        _logger('Ending session and restarting...\n\n')
+        if ENABLE_LOGGING:
+            log_file.close()
+        machine.reset()
 
 asyncio.run(main())
--- testing.py
+++ testing.py
@@ -1,7 +1,10 @@
 import time
 import machine
+import uasyncio as asyncio
 
+# MQ-9 Ro analog value: 132059, 0.641 V
 LED = machine.Pin("LED", machine.Pin.OUT)
+SENSOR = machine.ADC(machine.Pin(28))
 
 def _blinkLED(led, seconds):
     for i in range(int(seconds * 10)):
@@ -10,4 +13,11 @@ def _blinkLED(led, seconds):
         led.value(0)
         time.sleep_ms(50)
 
-_blinkLED(LED, 5)
+def read_gas_sensor():
+    analog_value = SENSOR.read_u16()  # Read the analog value (0 - 65535)
+    voltage = analog_value * 3.3 / 65535.0
+    return analog_value, voltage
+
+while True:
+    print(read_gas_sensor())
+    time.sleep_ms(200)
